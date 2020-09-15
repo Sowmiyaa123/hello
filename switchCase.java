bf623cc27c1a3d2f49eca51aaba591c461b97977
Sowmiyaa123 <70963714+Sowmiyaa123@users.noreply.github.com>
2020-09-15 13:58:13 +0530

    a program using loops
    for,do-while and switch case are used

---
+++ switchCase.java
@@ -0,0 +1,26 @@
+package SwitchCase;
+public class Switchcase{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int a,b,c;
+a=sc.nextInt();
+b=sc.nextInt();
+char ch;
+do{
+System.out.println("1.addition\t 2.subtraction\t");
+ch=sc.next().charAt(0);
+switch(ch){
+case 'A':
+casse 'a':
+System.out.println(a+b);
+break;
+case 'B':
+case 'b':
+System.out.println(a-b);
+break;
+default:
+system.out.println("invalid");\
+}
+}while(ch!='c');
+}
+}
